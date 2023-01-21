9e9c4cddc93ae799373ca871e823f3a901fca25f
Develing1991 <completed0728@gmail.com>
2023-01-21 14:20:55 +0900

    css:scrollTobehavior

--- main.js
+++ main.js
@@ -8,3 +8,13 @@ document.addEventListener("scroll", function () {
     navbarEl.classList.remove("navbar--dark");
   }
 });
+
+const navbarMenuEl = document.querySelector(".navbar__menu");
+navbarMenuEl.addEventListener("click", function (event) {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (!link) return;
+  console.log(event.target.dataset.link);
+  const scollTo = document.querySelector(link);
+  scollTo.scrollIntoView({ behavior: "smooth" });
+});
