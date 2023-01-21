d8a20dee743f9346293fe82296c702b95227af9b
Develing1991 <completed0728@gmail.com>
2023-01-21 14:25:32 +0900

    scrollintoview contactme 함수분리

--- main.js
+++ main.js
@@ -9,12 +9,23 @@ document.addEventListener("scroll", function () {
   }
 });
 
+// navbar 버튼 클릭 이동
 const navbarMenuEl = document.querySelector(".navbar__menu");
 navbarMenuEl.addEventListener("click", function (event) {
   const target = event.target;
   const link = target.dataset.link;
   if (!link) return;
-  console.log(event.target.dataset.link);
-  const scollTo = document.querySelector(link);
-  scollTo.scrollIntoView({ behavior: "smooth" });
+  scrollIntoView(link);
 });
+
+// home의 contact me 클릭 시 contact footer로 이동
+const homeContactEl = document.querySelector(".home__contact");
+homeContactEl.addEventListener("click", function (event) {
+  scrollIntoView("#contact");
+});
+
+// 스크롤 이동 공통함수로 분리
+function scrollIntoView(selector) {
+  const scollTo = document.querySelector(selector);
+  scollTo.scrollIntoView({ behavior: "smooth" });
+}
